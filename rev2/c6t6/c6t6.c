fd6330abbb7f76e5373afe85dd04cc054816c08b
T-H-Un <f.syun.b11@gmail.com>
2023-01-04 18:11:47 +0900

    new file:   rev2/c6t6/c6t6.c 	new file:   rev2/c6t6/c6t6.h 	new file:   rev2/c6t6/config.h 	new file:   rev2/c6t6/halconf.h 	new file:   rev2/c6t6/info.json 	new file:   rev2/c6t6/keymaps/all_keys/keymap.c 	new file:   rev2/c6t6/keymaps/default/keymap.c 	new file:   rev2/c6t6/keymaps/via/TH60JP_LAYOUT_rev2.json 	new file:   rev2/c6t6/keymaps/via/keymap.c 	new file:   rev2/c6t6/keymaps/via/rules.mk 	new file:   rev2/c6t6/mcuconf.h 	new file:   rev2/c6t6/rules.mk

---
+++ rev2/c6t6/c6t6.c
@@ -0,0 +1,2 @@
+#include "c6t6.h"
+
---
+++ rev2/c6t6/rules.mk
@@ -0,0 +1,42 @@
+# MCU name
+#MCU = STM32F303
+#BOARD = QMK_PROTON_C
+
+MCU = STM32F103
+#BOARD = genericSTM32F103C8
+
+##Change for STM32F103C6T6A
+# Bootloader selection
+OPT_DEFS += -DBOOTLOADER_STM32DUINO
+MCU_LDSCRIPT = STM32F103x6_stm32duino_bootloader
+BOARD = STM32_F103_STM32DUINO
+BOOTLOADER_TYPE = stm32duino
+DFU_ARGS = -d 1EAF:0003 -a 2 -R
+DFU_SUFFIX_ARGS = -v 1EAF -p 0003
+# LTO is required to fit the firmware into the available 24K of flash
+LTO_ENABLE = yes
+#Enable EEPROM emuration for STM32F103C6T6A
+EEPROM_DRIVER = vendor
+
+# Use SPI for RGB underglow:
+WS2812_DRIVER = spi
+
+# Build Options
+#   change yes to no to disable
+#
+BACKLIGHT_ENABLE = no
+BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = yes   # Mouse keys
+EXTRAKEY_ENABLE = yes   # Audio control and System control
+CONSOLE_ENABLE = no    # Console for debug
+COMMAND_ENABLE = no     # Commands for debug and configuration
+NKRO_ENABLE = yes           # Enable N-Key Rollover
+AUDIO_ENABLE = no
+RGBLIGHT_ENABLE = no   # Enable keyboard underlight functionality
+LEADER_ENABLE = yes
+
+OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
+
+LAYOUTS = 60_ansi 60_ansi_split_bs_rshift 60_iso 60_hhkb
+
+
